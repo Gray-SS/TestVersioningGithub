45665b26c2bd4e9a7edd71c87642cfa1659f3079
Maxime Bartha <maxime.bartha@gmail.com>
2021-09-29 15:47:16 +0200

    changement hello biche

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello people!");
+            Console.WriteLine("Hello biche !");
         }
     }
 }
